b4989fc5823c7b9e7a2be1b7be258dfac25fc913
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-09-04 23:25:26 +0200

    Update zadaca1.c

--- zadaca1.c
+++ zadaca1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Задача1
+// Внеси преку тастатура два цели броеви a и b. Отпечати ги броевите на
+// екран,најди ја нивата разлика и отпечати ја разликата.
+
+int main()
+{
+    int number1,number2,result;
+
+    scanf("%d", &number1);
+    scanf("%d", &number2);
+
+    result = number1-number2;
+
+    printf("The numbers are %d and %d\n",number1,number2);
+    printf("The result is: %d", result);
+
+    return 0;
+}
