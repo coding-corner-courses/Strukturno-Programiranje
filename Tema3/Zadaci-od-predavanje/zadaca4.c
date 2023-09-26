ef8533a4417c167cecfde48752c8f240b1e95580
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-09-27 00:29:20 +0200

    Create zadaca4.c

---
+++ Tema3/Zadaci-od-predavanje/zadaca4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//Напишете програма која ќе проверува дали внесен број е делив со 3, 5 ,7 или 11. Програмата соодветно го печати бројот и неговиот делител.
+//Доколку бројот не е делив со ниеден од наведените броеви да се отпечати “The number is not divisible with the expected numbers”.
+int main()
+{
+
+    int number;
+
+    scanf("%d", &number);
+
+    if(number % 3 == 0)
+    {
+        printf("The number is divisible with three");
+    }
+    else if(number % 5 ==0)
+    {
+        printf("The number is divisible with five");
+    }
+    else if(number % 7 == 0)
+    {
+        printf("The number is divisible with seven");
+    }
+    else if(number % 11 ==0)
+    {
+        printf("The number is divisble with eleven");
+    }
+    else
+    {
+        printf("The number is not divisible with the expected numbers");
+    }
+}
