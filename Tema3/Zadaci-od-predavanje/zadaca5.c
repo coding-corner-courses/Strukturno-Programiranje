c22b3a4359ba8114bb0ba6cec924edbe7aa4f318
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-09-27 00:46:05 +0200

    Create zadaca5.c

---
+++ Tema3/Zadaci-od-predavanje/zadaca5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Напишете програма која ќе проверува дали внесен карактер е голема буква. Отпечатете го резултатот на екран.
+// [65, 90] во ASCII
+int main()
+{
+
+    char character;
+
+    scanf("%c", &character);
+
+    if((int)character >= 65 && (int) character <= 90){
+        printf("The character is upper letter");
+    }
+}
