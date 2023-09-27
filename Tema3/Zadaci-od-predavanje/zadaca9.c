3bcc593dd701e1047bd4875ef7eef4dfef3efe54
coding-corner-courses <143335964+coding-corner-courses@users.noreply.github.com>
2023-09-28 01:39:35 +0200

    Create zadaca9.c

---
+++ Tema3/Zadaci-od-predavanje/zadaca9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//Напишете програма која ќе претставува калкулатор и за внесен аритметчки оператор ќе враќа резултат.
+int main()
+{
+
+    // +, -, *, /
+    char arithmeticOperator;
+    int number1, number2;
+
+    scanf("%c",&arithmeticOperator);
+    scanf("%d",&number1);
+    scanf("%d",&number2);
+
+    double result;
+
+    switch(arithmeticOperator){
+        case '+':
+            result=number1+number2;
+            break;
+        case '-':
+            result=number1-number2;
+            break;
+        case '*':
+            result=number1*number2;
+            break;
+        case '/':
+            result=(double)number1/number2;
+            break;
+    }
+
+    printf("Result %.2f", result);
+
+    return 0;
+}
